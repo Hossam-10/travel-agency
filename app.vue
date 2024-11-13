be736f37380bd8b42f701580e04b505392798b50
Hossam-M10 <hossam.mahmoud@dafagate.com>
2024-11-13 03:33:38 +0200

    Remove detect browser language to fix default locale

--- app.vue
+++ app.vue
@@ -16,10 +16,14 @@ useHead({
 });
 
 //get locale from cookies and set it to i18n
+//and if no cookie set default locale to ar
 const { setLocale } = useI18n();
 const localCookie = useCookie("lang");
 if (localCookie.value) {
   setLocale(localCookie.value as string & ("en" | "ar"));
+} else {
+  setLocale("ar");
+  localCookie.value = "ar";
 }
 
 //set locale html attributes
